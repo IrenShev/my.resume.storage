cf5c3484cc0fb5bb4a993470a1be2742dadbbf52
Stas <w3oks@mail.ru>
2017-08-03 12:44:56 +0300

    HW4: lambdas, add constructor

--- src/main/java/examples/lambdas/Loader.java
+++ src/main/java/examples/lambdas/Loader.java
@@ -2,12 +2,10 @@ package examples.lambdas;
 
 public class Loader {
     public static void main(String[] args) {
-        Switcher sw = new Switcher();
+
         Lamp lamp = new Lamp();
         Radio radio = new Radio();
-        sw.addElListener(lamp);
-        sw.addElListener(radio);
-        //sw.addElListener(null);
+        Switcher sw = new Switcher(lamp,radio);
         sw.switchOn();
     }
 }
--- src/main/java/examples/lambdas/Switcher.java
+++ src/main/java/examples/lambdas/Switcher.java
@@ -1,10 +1,19 @@
 package examples.lambdas;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Switcher {
-    List<ElectrisityConsumer> consumers = new ArrayList<>();
+    List<ElectrisityConsumer> consumers;
+
+    public Switcher(ElectrisityConsumer ... consumers){
+        this.consumers = Arrays.asList(consumers);
+    }
+    public Switcher(List<ElectrisityConsumer> consumers) {
+        this.consumers = consumers;
+    }
 
     public void addElListener(ElectrisityConsumer listener){
         consumers.add(listener);
