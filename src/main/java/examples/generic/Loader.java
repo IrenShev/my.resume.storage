cfbc6c1135527dbd459fadae47b6ce56d02c2fce
Stas <w3oks@mail.ru>
2017-08-15 16:48:09 +0300

    HW5: example use Object

---
+++ src/main/java/examples/generic/Loader.java
@@ -0,0 +1,28 @@
+package examples.generic;
+
+public class Loader{
+    public static void main(String[] args) {
+        BoxPrinter value1 = new BoxPrinter(new Integer(10));
+        System.out.println(value1);
+        Integer intValue1 = (Integer) value1.getValue();
+        BoxPrinter value2 = new BoxPrinter("Hello world");
+        System.out.println(value2);
+
+    }
+
+}
+class BoxPrinter {
+    private Object val;
+
+    public BoxPrinter(Object arg) {
+        val = arg;
+    }
+
+    public String toString() {
+        return "{" + val + "}";
+    }
+
+    public Object getValue() {
+        return val;
+    }
+}
